e934845e688af89911149e832de63e4f00250875
kibaamor <kibaamor@gmail.com>
2021-10-25 23:18:38 +0800

    idle check

--- examples/server/secho_conn.h
+++ examples/server/secho_conn.h
@@ -4,6 +4,8 @@
 
 using namespace knet;
 
+constexpr int64_t TIMER_ID_IDLE_CHECK = 9999;
+
 class secho_conn : public echo_conn {
 public:
     using echo_conn::echo_conn;
@@ -12,31 +14,37 @@ private:
     void do_on_connected() override
     {
         echo_conn::do_on_connected();
-        set_idle_timer();
+        if (mgr.max_idle_ms) {
+            add_timer(knet::now_ms() + mgr.max_idle_ms, TIMER_ID_IDLE_CHECK);
+        }
     }
     void do_on_timer(int64_t ms, const knet::userdata& ud) override
     {
         echo_conn::do_on_timer(ms, ud);
 
-        const auto now = knet::now_ms();
-        if (_last_recv_ms && now > _last_recv_ms + mgr.max_idle_ms) {
-            mgr << "kick client: " << get_connid()
-                << ", last_recv_ms: " << _last_recv_ms
-                << ", now_ms: " << now
-                << ", delta_ms: " << now - _last_recv_ms << "\n";
+        if (ud.data.i64 == TIMER_ID_IDLE_CHECK) {
+            const auto now = knet::now_ms();
+            if (!_last_recv_ms) {
+                _last_recv_ms = now;
+            } else if (now > _last_recv_ms + mgr.max_idle_ms) {
+                mgr << "kick client: " << get_connid()
+                    << ", last_recv_ms: " << _last_recv_ms
+                    << ", now_ms: " << now
+                    << ", delta_ms: " << now - _last_recv_ms << "\n";
 
-            if (!get_connid()) {
-                std::cerr << "connid: " << get_connid()
-                          << ", last_recv_ms: " << _last_recv_ms
-                          << ", now_ms: " << now
-                          << ", delta_ms: " << now - _last_recv_ms << "\n";
-            } else {
-                disconnect();
-                return;
+                if (!get_connid()) {
+                    std::cerr << "connid: " << get_connid()
+                              << ", last_recv_ms: " << _last_recv_ms
+                              << ", now_ms: " << now
+                              << ", delta_ms: " << now - _last_recv_ms << "\n";
+                } else {
+                    disconnect();
+                    return;
+                }
             }
-        }
 
-        set_idle_timer();
+            add_timer(knet::now_ms() + mgr.max_idle_ms, TIMER_ID_IDLE_CHECK);
+        }
     }
 
     size_t do_on_recv_data(char* data, size_t size) override
@@ -66,7 +74,7 @@ private:
     void set_idle_timer()
     {
         if (mgr.max_idle_ms) {
-            add_timer(knet::now_ms() + mgr.max_idle_ms, 0);
+            add_timer(knet::now_ms() + mgr.max_idle_ms, TIMER_ID_IDLE_CHECK);
         }
     }
 

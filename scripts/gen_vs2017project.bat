06af465f17c92993f737eccac35c0705ee02f58e
kibaamor <kibaamor@gmail.com>
2021-10-25 15:50:28 +0800

    remove scripts

--- scripts/gen_vs2017project.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cmake . -B build -G "Visual Studio 15 2017 Win64"
-cmake --open build
--- scripts/memcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-origins=yes --track-fds=yes $@
